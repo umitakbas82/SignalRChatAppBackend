058cccef7310cce1f370ae520663380227ef00ed
UmitAkbas <umit.akbas@hotmail.com.tr>
2023-11-07 06:35:13 +0300

    adding hub class

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignarRChatAppBackend.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SignarRChatAppBackend.Hubs
+{
+    public class ChatHub:Hub
+    {
+        private readonly ChatService _chatService;
+        public ChatHub(ChatService chatService) 
+        {
+            _chatService = chatService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Come2Chat");
+            await Clients.Caller.SendAsync("UserConnected");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+
+   
+
+}
